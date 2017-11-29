4d1c422d9d5ef0bf62d6265ad265f59435732c75
alexantaki16 <alexander.antaki1@marist.edu>
2017-11-29 13:37:18 -0800

    Update tools.php

--- tools.php
+++ tools.php
@@ -1,5 +1,5 @@
 <?php
-# this function makes sure the name is valid
+# this function makes sure the name and is valid
 function valid_name($name){
   for($i = 0; $i < strlen($name); $i++){
     if( is_numeric( $name[$i] ) ){
@@ -8,12 +8,10 @@ function valid_name($name){
   }
   return true;
 }
-
+# This function makes sure the email is valid
 function valid_email($name){
-  if($name contains '@'){
+  if(strpos($a, '@') !== false)
     return true;
-  }else{
+  else
     return false;
-  }
 }
-?>
