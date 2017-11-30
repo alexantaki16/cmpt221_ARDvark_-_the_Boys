67c064efa4cc916f2c0c5f8e4c2e11892411caee
Matthew <matthew.oakley1@marist.edu>
2017-11-30 00:59:04 -0500

    needs for each file to do commands

---
+++ includes/tools.php
@@ -0,0 +1,59 @@
+<?php
+  
+  # sterilize the users input
+  function sterilize($input){
+    return htmlspecialchars($input);
+  }
+  
+  # this function makes sure the name and is valid
+  function valid_name($name){
+    for($i = 0; $i < strlen($name); $i++){
+      if( is_numeric( $name[$i] ) ){
+        return False;
+      }
+    }
+    return True;
+  }
+  
+  # This function makes sure the email is valid
+  function valid_email($name){
+    if(strpos($name, '@') !== false)
+      return true;
+    else
+      return false;
+  }
+  
+  # makes sure the username and password entered are valid
+  function adminLogin($username, $password){
+    
+    $query = 'SELECT pass FROM users WHERE username = '.$username;    
+    
+    $results = mysqli_query( $dbc , $query );
+    
+    $row = mysqli_fetch_array( $results , MYSQLI_ASSOC );
+    
+    return password_verify($password, $row['pass']);
+  }
+  /*
+  function insertIntoTable($create_date, $room, $status, $description){
+    $query = 'INSERT INTO stuff($create_date, $room, $status, $description)
+    VALUES($dateFound, $locationFound, $status, $item)';
+    echo "Thank you for your submission.  Your input has been processed!";
+  }
+  */
+  
+  function insertItem($dbc, $name, $eMail, $item, $itemType, $desc, $status){
+    $query = 'INSERT INTO stuff(pName, E_Mail, itemName, catagory, description, status) VALUES ("'.$name.'","'.$eMail.'","'.$item.'","'.$itemType.'","'.$desc.'","'.$status.'")';
+    
+    return $results = mysqli_query($dbc, $query);
+    
+  }
+  
+  # TODO LOOK into syntax
+  function deleteFromTable($id){
+    $query = 'DELETE FROM stuff WHERE sid = '.$id;
+    
+    $results = mysqli_query( $dbc , $query);
+    
+    return $results;
+  }
