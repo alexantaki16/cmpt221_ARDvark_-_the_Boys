ec8d806c3837356fed265ac2c14a118b07e480b5
Matthew <matthew.oakley1@marist.edu>
2017-11-29 23:42:00 -0500

    made it a beaut

--- tools.php
+++ tools.php
@@ -1,33 +1,45 @@
 <?php
-# this function makes sure the name and is valid
-function valid_name($name){
-  for($i = 0; $i < strlen($name); $i++){
-    if( is_numeric( $name[$i] ) ){
-      return false;
+
+  # this function makes sure the name and is valid
+  function valid_name($name){
+    for($i = 0; $i < strlen($name); $i++){
+      if( is_numeric( $name[$i] ) ){
+        return false;
+      }
     }
-  }
-  return true;
-}
-# This function makes sure the email is valid
-function valid_email($name){
-  if(strpos($a, '@') !== false)
     return true;
-  else
-    return false;
-}
-#PSEDUO
-function adminLogin(){
-  $query = 'SELECT username FROM users WHERE username = "caz", pass = "doctorwho112" AND confPass = "doctorwho112"';
-}
+  }
+  
+  # This function makes sure the email is valid
+  function valid_email($name){
+    if(strpos($a, '@') !== false)
+      return true;
+    else
+      return false;
+  }
 
-function insertIntoTable($create_date, $room, $status, $description){
-  $query = 'INSERT INTO stuff($create_date, $room, $status, $description)
-  VALUES($dateFound, $locationFound, $status, $item)';
-  echo "Thank you for your submission.  Your input has been processed!";
-}
+  # makes sure the username and password entered are valid
+  function adminLogin($username, $password){
+    
+    $query = 'SELECT pass FROM users WHERE username = '.$username;    
+    
+    $results = mysqli_query( $dbc , $query );
+    
+    $row = mysqli_fetch_array( $results , MYSQLI_ASSOC )
+    
+    $isPass = password_verify($password, $row['pass']);
+    
+    return $isPass;
+  }
 
-function deleteFromTable($create_date, $room, $status, $description){
-  $query = 'DELETE FROM stuff($dateFound, $location, $status, $description)
-  WHERE status = 'claimed'';
-  echo "Thank you!  The item has been deleted from the table.";
-}
+  function insertIntoTable($create_date, $room, $status, $description){
+    $query = 'INSERT INTO stuff($create_date, $room, $status, $description)
+    VALUES($dateFound, $locationFound, $status, $item)';
+    echo "Thank you for your submission.  Your input has been processed!";
+  }
+
+  function deleteFromTable($create_date, $room, $status, $description){
+    $query = 'DELETE FROM stuff($dateFound, $location, $status, $description)
+    WHERE status = 'claimed'';
+    echo "Thank you!  The item has been deleted from the table.";
+  }
