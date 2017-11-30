74c3c35b5bcdbbe3308efb864366c676f55acf97
alexantaki16 <alexander.antaki1@marist.edu>
2017-11-29 22:53:07 -0500

    Update sanitation.php

--- sanitation.php
+++ sanitation.php
@@ -1,5 +1,8 @@
 <?php
   $con = mysqli_connect("localhost", "my_user", "my_password", "my_db");
+  if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
   $yourName = mysqli_real_escape_string($con, $_POST['yourName']);
   $eMail = mysqli_real_escape_string($con, $_POST['eMail']);
   $nameOfItem = mysqli_real_escape_string($con, $_POST['nameOfItem']);
