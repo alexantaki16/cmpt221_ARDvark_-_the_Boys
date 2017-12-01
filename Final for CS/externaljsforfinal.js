8e964dc1712d2a1bfb7b153af3336620eed057d3
alexantaki16 <alexander.antaki1@marist.edu>
2017-12-01 09:34:02 -0800

    Add files via upload
    Don't use

---
+++ Final for CS/externaljsforfinal.js
@@ -0,0 +1,15 @@
+function teamhistoryfunfact() {
+	document.getElementById("funfact").innerHTML = "The mother of Michael Conforto, a young slugger on the Mets, has two Olympic gold medals";
+}
+function championshipfunfact() {
+	document.getElementById("funfact2").innerHTML = "  In the 1986 World Series, they Mets lost the first game by one and the second game six."
+	+ "<br/>" + "In the 2015 World Series, the Mets also lost the first game by one and the second game by six as well.";
+}
+function halloffamersfunfact() {
+	document.getElementById("funfact3").innerHTML = "The year the Mets won their first World Series in 1969, they also " +
+	"recorded their first winning season in their history.";
+}
+function whyimafanfunfact(){
+	document.getElementById("funfact4").innerHTML = "Abner Doubleday invented the game of baseball in 1839 in Cooperstown, New York. " +
+	" He was also a Civil War hero.";
+}
