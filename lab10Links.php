0c7d28ac74596912b1a6c3fe59b4af9873291235
Johnathan Clementi <johnclementi@148-100-128-97.FoxNet.marist.edu>
2017-11-30 23:34:23 -0500

    Links Example

---
+++ lab10Links.php
@@ -0,0 +1,104 @@
+<!--
+This PHP script front-ends linkyprints.php with a login page.
+Originally created By Ron Coleman.
+Revision history:
+Who	Date		Comment
+RC  07-Nov-13   Created.
+-->
+<!DOCTYPE html>
+<html>
+<?php
+# Connect to MySQL server and the database
+require( 'includes/connect_db.php' ) ;
+
+# Connect to MySQL server and the database
+require( 'presidents_login_tools.php' ) ;
+
+if ($_SERVER[ 'REQUEST_METHOD' ] == 'POST') {
+
+	#The next two variables will take and validate the input
+	$lname = $_POST['lname'] ;
+
+    $num = validate($lname) ;
+
+    if($num == -1)
+      echo '<P style=color:red>Login failed please try again.</P>' ;
+
+    else
+      load('linkypresidents.php', $num);
+}
+?>
+<!-- Get inputs from the user. -->
+<h1>Presidents login</h1>
+<form action="presidents_login.php" method="POST">
+<table>
+<tr>
+<td>Last Name:</td><td><input type="text" name="lname"></td>
+</tr>
+</table>
+<p><input type="submit" ></p>
+</form>
+</html>
+
+
+Presidents_login_tools.php
+
+	<!--
+This file contains PHP login helper functions.
+Orginally created by Ron Coleman.
+History:
+Who	Date		Comment
+RC	 7-Nov-13	Created.
+-->
+<?php
+# Includes these helper functions
+require( 'includes/helpers.php' ) ;
+
+# Loads a specified or default URL.
+function load( $page = 'linkypresidents.php', $num = -1 )
+{
+  # Begin URL with protocol, domain, and current directory.
+  $url = 'http://' . $_SERVER[ 'HTTP_HOST' ] . dirname( $_SERVER[ 'PHP_SELF' ] ) ;
+
+  # Remove trailing slashes then append page name to URL and the print id.
+  $url = rtrim( $url, '/\\' ) ;
+  $url .= '/' . $page . '?num=' . $num;
+
+  # Execute redirect then quit.
+  session_start( );
+
+  header( "Location: $url" ) ;
+
+  exit() ;
+}
+
+# Validates the print name.
+# Returns -1 if validate fails, and >= 0 if it succeeds
+# which is the primary key id.
+function validate($prname = '')
+{
+    global $dbc;
+
+    if(empty($prname))
+      return -1 ;
+
+    # Make the query
+    $query = "SELECT num, lname FROM presidents WHERE lname='" . $prname . "'" ;
+    show_query($query) ;
+
+    # Execute the query
+    $results = mysqli_query( $dbc, $query ) ;
+    check_results($results);
+
+    # If we get no rows, the login failed
+    if (mysqli_num_rows( $results ) == 0 )
+      return -1 ;
+
+    # We have at least one row, so get the frist one and return it
+    $row = mysqli_fetch_array($results, MYSQLI_ASSOC) ;
+
+    $num = $row [ 'num' ] ;
+
+    return intval($num) ;
+}
+?>
